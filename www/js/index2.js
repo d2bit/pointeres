2e77d9e5ebccce88008fbecec9adc79c75dcc82f
David BF <burretfresket@gmail.com>
2014-03-30 14:55:34 +0200

    pantalles poi y menu-drcha disenyades. clics funcionant. iscroll no funciona.

--- www/js/index2.js
+++ www/js/index2.js
@@ -1,37 +1,73 @@
 $(window).on('load', onLoad);
 
 var myScroll;
+var whereiam;
 
 function onLoad() {
-	alert("onLoad...");
+	// alert("onLoad...");
 	$().ready(function() {
 		document.addEventListener("deviceready",
 			onDeviceReady, false);
+        document.addEventListener("backbutton",
+            onBackButton, false);
 	});
     // onDeviceReady(); //sobra
 
-    // FastClick.attach(document.body);
+    FastClick.attach(document.body);
 
 
     // EVENTOS
-    $('#header').on('tap', function(evt) {
+    $('.icon-menu').on('tap', function(evt) {
         if ($('#principal').hasClass('center')) {
+            whereiam = 'menu';
             $('#principal').removeClass('center').addClass('right');
             $('#menu').removeClass('left').addClass('center');
         }
         else {
+            whereiam = undefined;
             $('#principal').removeClass('right').addClass('center');
             $('#menu').removeClass('center').addClass('left');
         }
     });
 
     $('#nav_principal > li').on('tap', function(evt) { 
-        alert(this.innerText);
+        // alert(this.innerText, $(this).attr('data-menu'));
+
+        if (whereiam !== 'menu') {
+            whereiam = $(this).attr('data-menu');
+            if (whereiam) {
+                $('#principal').removeClass('center').addClass('left');
+                $('#' + whereiam).removeClass('right').addClass('center');
+            }
+        }
     });
+
+    $('.boton-atras').on('tap', function(evt) {
+        volverAtras();
+    })
 }
-
+function volverAtras() {
+    switch (whereiam) {
+        case 'menu':
+            $('#principal').removeClass('right').addClass('center');
+            $('#menu').removeClass('center').addClass('left');
+            break;
+        case undefined:
+            return true;
+        default:
+            $('#principal').removeClass('left').addClass('center');
+            $('#' + whereiam).removeClass('center').addClass('right');
+    }
+    whereiam = undefined;
+}
 function onDeviceReady() {
-	alert('onDeviceReady...');
+	// alert('onDeviceReady...');
 
-    myScroll = new IScroll('#wrapper');
+    myScroll = new IScroll('#principal');
+}
+function onBackButton(evt) {
+    // alert('onBackButton');
+    if (volverAtras()) { // si estem en la principal tanquem la app
+        navigator.app.exitApp();
+    }
 }
